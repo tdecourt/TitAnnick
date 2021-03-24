aa7b511290acbfa5e2acde81dd35ae47f38c5678
gael.mousset <gael.mousset@etudiant.univ-lr.fr>
2021-03-24 17:35:11 +0100

    feat(app): ajout composent detail-bateau-head

---
+++ app/src/app/detail-bateau-head/detail-bateau-head.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-bateau-head',
+  templateUrl: './detail-bateau-head.component.html',
+  styleUrls: ['./detail-bateau-head.component.css']
+})
+export class DetailBateauHeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
