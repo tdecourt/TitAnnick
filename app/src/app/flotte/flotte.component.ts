d51efcc0a3f59a3cfb540b3d7836726d6f3c2336
tdecourt <thomas.decourty@etudiant.univ-lr.fr>
2021-03-24 10:13:47 +0100

    feat(app): ajout La Flotte

--- app/src/app/flotte/flotte.component.ts
+++ app/src/app/flotte/flotte.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { bateaux } from "../bateaux";
 
 @Component({
-  selector: 'app-flotte',
-  templateUrl: './flotte.component.html',
-  styleUrls: ['./flotte.component.css']
+    selector: 'app-flotte',
+    templateUrl: './flotte.component.html',
+    styleUrls: ['./flotte.component.css']
 })
 export class FlotteComponent implements OnInit {
-  bateaux = bateaux;
-  constructor() {}
+    bateaux = bateaux;
+    constructor() {}
 
-  ngOnInit() {}
+    ngOnInit() {}
 }
