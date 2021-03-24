bd479624f9d944d678433fac1f32bff6eaf895aa
tdecourt <thomas.decourty@etudiant.univ-lr.fr>
2021-03-24 23:14:10 +0100

    feat(api): ajout route detailBateau

---
+++ api/src/Controller/BateauController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BateauRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\Annotations as REST;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+
+class BateauController extends AbstractController
+{
+    /**
+     * @Rest\View()
+     * @Rest\Get(
+     *      "/api/bateaux/{id}",
+     *      name="detailBateau"
+     * )
+     */
+    public function detailBateau(
+        Request $request,
+        BateauRepository $repo
+    )
+    {
+        $bateau = $repo->find($request->get('id'));
+        return $bateau;
+    }
+}
