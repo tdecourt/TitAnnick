b86b927ef9b7812eb14c305ede0947651457ece2
Mousset Gael <gael.mousset@etudiant.univ-lr.fr>
2021-03-30 13:53:04 +0200

    fix : cangemant nom component detail-bateau-head en bateau-detail

--- app/src/app/bateau-detail/bateau-detail.component.spec.ts
+++ app/src/app/bateau-detail/bateau-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DetailBateauHeadComponent } from './detail-bateau-head.component';
+import { BateauDetailComponent } from './bateau-detail.component';
 
-describe('DetailBateauHeadComponent', () => {
-  let component: DetailBateauHeadComponent;
-  let fixture: ComponentFixture<DetailBateauHeadComponent>;
+describe('BateauDetailComponent', () => {
+  let component: BateauDetailComponent;
+  let fixture: ComponentFixture<BateauDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailBateauHeadComponent ]
+      declarations: [ BateauDetailComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailBateauHeadComponent);
+    fixture = TestBed.createComponent(BateauDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
