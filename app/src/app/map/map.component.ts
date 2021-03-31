87b0d464bc9718466afe6c40b8e8b047299bfb88
Alexis Leguennec <alexis.le_guennec@etudiant.univ-lr.fr>
2021-03-31 17:34:36 +0200

    fix(map): Suppression du controlBar

--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -13,8 +13,8 @@ import * as mapboxgl from 'mapbox-gl';
 
   private map: mapboxgl.Map | undefined;
   style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 46.1558;
-  lng = -1.1532;
+  lat = 46.1510;
+  lng = -1.1515;
 
   constructor() { }
 
@@ -24,11 +24,9 @@ import * as mapboxgl from 'mapbox-gl';
     this.map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
-      zoom: 13,
+      zoom: 15.75,
       center: [this.lng, this.lat]
     });
-    // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
   }
 }
 
