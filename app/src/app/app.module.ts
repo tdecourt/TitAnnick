2a0ea617890a74ae1cfe80e67332b79fe52d7053
Matthieu Chambon <matthieu.chambon@etudiant.univ-lr.fr>
2021-04-05 20:03:57 +0200

    api meteo works

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FlotteComponent } from './flotte/flotte.component';
 import { GalerieComponent } from './galerie/galerie.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { AccueilHeaderComponent } from './accueil-header/accueil-header.component';
+import { MeteoComponent } from './meteo/meteo.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { AccueilHeaderComponent } from './accueil-header/accueil-header.componen
     FlotteComponent,
     GalerieComponent,
     MenuBarComponent,
-    AccueilHeaderComponent
+    AccueilHeaderComponent,
+    MeteoComponent
   ],
 
   imports: [
